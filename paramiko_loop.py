94646afec74b7f328d57964159353d11190e5881
mehdi-azrabi <mr.azrabi@gmail.com>
2022-07-20 23:35:48 +1000

    commit all

---
+++ paramiko_loop.py
@@ -0,0 +1,33 @@
+import paramiko
+import time
+from getpass import getpass
+
+ip = "192.168.100.251"
+username = "admin"
+password = input("Enter your password: ")
+
+
+SESSION = paramiko.SSHClient()
+SESSION.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+SESSION.connect(ip,port=22,
+                username=username,
+                password=password,
+                look_for_keys=False,
+                allow_agent=False)
+
+DEVICE_ACCESS = SESSION.invoke_shell()
+DEVICE_ACCESS.send(b'config t\n')
+for i in range(1, 10):
+    DEVICE_ACCESS.send('int lo ' +str(i) + '\n')
+    DEVICE_ACCESS.send('ip address 1.1.1.'+str(i) +' 255.255.255.255\n')
+ 
+
+time.sleep(3)
+
+DEVICE_ACCESS.send(b'do show ip int bri\n \n \n')
+time.sleep(3)
+output = DEVICE_ACCESS.recv(65000)
+print (output.decode('ascii'))
+SESSION.close
+
+
--- subnet.py
+++ subnet.py
@@ -3,3 +3,10 @@ subnet = '192.168.1.'
 for n in range(1, 20):
     print(subnet + str(n))
 
+device = ['R1', 'R2', 'SW1', 'SW2']
+dc = ['dc01.com', 'dc02.com']
+for i in device:
+    for j in dc:
+        print(i + '.' + j)
+
+
